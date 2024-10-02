653982ba1d8a0d7be986d129143313d1a2fbf66d
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-10-02 17:00:09 +0530

    Update ques42.cpp

--- Digital_Questions_PYQ/ques42.cpp
+++ Digital_Questions_PYQ/ques42.cpp
@@ -1,5 +1,22 @@
 // Question 42
-
+// QUESTION NO : 42
+// Given a string Str, which contains numbers (0 to 9)and also letters of the English and
+// to 'Z'). The task is to reverse string the string in such a way that the positiors of
+// numbers in the string are left unaltered.
+// Example 1:
+// Input:
+// a1b2igh3 -> Value of Str
+// Output:
+// h1g2iba3
+// Explanation:
+// Without changing the positions of 1, 2 and 3, only the positions of characters 'h', 'g,
+// "i', 'b' and 'a' have been reversed.
+// QUESTION NO : 42
+// Example 2:
+// Input:
+// Ab5c7de96 -> Value of Str
+// Output:
+// ed5c7bA96
 #include<iostream>
 #include<cctype>
 using namespace std;
